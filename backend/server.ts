98fc42ffaaedf5c5a74a4d2892b400c5c364c1eb
Ania-beigi <anahita.einbeigi@gmail.com>
2025-05-26 10:25:38 +0200

    i fixed cors-policy

--- backend/server.ts
+++ backend/server.ts
@@ -14,7 +14,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+
+const allowedOrigins = [
+  'https://www.resourcepoint.se'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'CORS-policy: Denna origin är inte tillåten.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -236,5 +251,5 @@ app.post('/api/add-position', async (req: Request, res: Response) => {
 
 // -------- Starta servern --------
 app.listen(PORT, () => {
-  console.log(`Servern är igång: http://localhost:${PORT}`);
+  console.log(`Servern är igång på port ${PORT}`);
 });
--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -24,8 +24,7 @@ const Contact = () => {
     e.preventDefault();
   
     try {
-      // const response = await fetch("https://resourcepoint.se/api/contact", {
-
+      // const response = await fetch("https://www.resourcepoint.se/api/contact",{
       const response = await fetch("http://localhost:5000/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
